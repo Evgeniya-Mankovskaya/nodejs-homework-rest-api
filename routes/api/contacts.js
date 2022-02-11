69245dfd6bdbbe5975aa5894a46de1377b0e99db
Yevheniia Mankivska <evgeniyamankovskaya@gmail.com>
2022-02-11 19:03:03 +0200

    hw-03

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const { validation, ctrlWrapper } = require("../../middlewares");
-const { contactSchema } = require("../../schemas");
+const { joiSchema, favoriteJoiSchema } = require("../../models/contact");
 const { contacts: ctrl } = require("../../controllers");
 
 router.get("/", ctrlWrapper(ctrl.listContacts));
 
 router.get("/:contactId", ctrlWrapper(ctrl.getContactById));
 
-router.post("/", validation(contactSchema), ctrlWrapper(ctrl.addContact));
+router.post("/", validation(joiSchema), ctrlWrapper(ctrl.addContact));
 
 router.delete("/:contactId", ctrlWrapper(ctrl.removeContact));
 
 router.put(
   "/:contactId",
-  validation(contactSchema),
+  validation(joiSchema),
   ctrlWrapper(ctrl.updateContact)
+);
+
+router.patch(
+  "/:contactId/favorite",
+  validation(favoriteJoiSchema),
+  ctrlWrapper(ctrl.updateStatusContact)
 );
 
 module.exports = router;
